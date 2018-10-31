0b9d3f91bae10dfca10aa6793e231f3b8abcec93
Jerome <654321su@gmail.com>
2018-10-31 11:58:15 +0900

    Add Vuetube Logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <img style="width: 140px; margin: 14px;" src="./assets/logo.png" alt="logo">
     <VideoPlayer />
   </div>
 </template>
@@ -20,8 +21,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
